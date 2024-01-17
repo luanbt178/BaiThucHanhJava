9aed02ea2c78802e999c1f05112aeca1fdb37684
luanbt178 <156191805+luanbt178@users.noreply.github.com>
2024-01-17 23:43:06 +0700

    Add files via upload

--- GiaiPTbac1.java
+++ GiaiPTbac1.java
@@ -1,7 +1,23 @@
 package BaithuchanhJava;
-
+import java.util.Scanner;
 public class GiaiPTbac1 {
-    System.input
+    public static void main(String[] args) {
+        float a,b,kq;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap gia tri cua a: ");
+        a = input.nextFloat();
+        System.out.print("Nhap gia tri cua b: ");
+        b = input.nextFloat();
+        if (a == 0)
+        {
+            System.out.println("Khong phai phuong trinh bac nhat");
+        }
+        else
+        {
+            kq = -b / a;
+            System.out.println("Phuong trinh co nghiem x = "+ kq);
+        }
+    }
 
 
 }
